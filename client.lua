ffd258200ec427fb1df840276aca54f647280ffd
MiamiScripts <67082596+MiamiScripts@users.noreply.github.com>
2020-10-28 09:27:21 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,20 +4,13 @@ Citizen.CreateThread(function()
 })
 end)
 
+blipActive = false
+
 RegisterCommand('setcarmeet', function(source, args)
 	if args[1] == nil then
 		TriggerEvent('notification', 'You must enter a time', 2)
 	else
-		local targetCoords = GetEntityCoords(PlayerPedId())
-		local blip = AddBlipForCoord(targetCoords.x, targetCoords.y, targetCoords.z)
-
-		SetBlipSprite (blip, Config.Blip.Sprite)
-		SetBlipScale  (blip, Config.Blip.Scale)
-		SetBlipColour (blip, Config.Blip.Colour)
-		SetBlipAsShortRange(blip, true)
-
-		BeginTextCommandSetBlipName(Strings['blip_name'])
-		EndTextCommandSetBlipName(blip)
+		blipActive = true
 		TriggerServerEvent("miamicarmeets:chat")
 		Citizen.Wait(args[1] * 60000/4)
 		TriggerServerEvent("miamicarmeets:chat")
@@ -27,7 +20,24 @@ RegisterCommand('setcarmeet', function(source, args)
 		TriggerServerEvent("miamicarmeets:chat")
 		Citizen.Wait(args[1] * 60000/4)
 		TriggerServerEvent("miamicarmeets:chat")
-		RemoveBlip(blip)
+		blipActive = false
 
 	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+			if blipActive then
+				local targetCoords = GetEntityCoords(PlayerPedId())
+				local blip = AddBlipForCoord(targetCoords.x, targetCoords.y, targetCoords.z)
+
+				SetBlipSprite (blip, Config.Blip.Sprite)
+				SetBlipScale  (blip, Config.Blip.Scale)
+				SetBlipColour (blip, Config.Blip.Colour)
+				SetBlipAsShortRange(blip, true)
+
+				BeginTextCommandSetBlipName(Strings['blip_name'])
+				EndTextCommandSetBlipName(blip)
+			end
 end)
